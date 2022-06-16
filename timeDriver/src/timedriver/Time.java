688cb7fc7fe02dbd449c7d87497b2c8f0a1479f9
Kevin Bell <bellKevin@pm.me>
2022-06-16 20:11:04 +0000

    Update Time.java

--- timeDriver/src/timedriver/Time.java
+++ timeDriver/src/timedriver/Time.java
@@ -2,23 +2,34 @@ package timedriver;
 
 class Time {
 
-    Time(int hours) {
+    private int hours, minutes, seconds;
+    private String AMorPM;
 
+    Time(int hours) {
+        this.hours = hours;
     }
 
     Time(int hours, int minutes) {
-
+        this.hours = hours;
+        this.minutes = minutes;
     }
 
     Time(int hours, int minutes, int seconds) {
-
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
     }
 
-    Time(int hours, int minutes, int seconds, String am) {
-
+    Time(int hours, int minutes, int seconds, String AMorPM) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+        if (AMorPM.equalsIgnoreCase("PM")) {
+            this.hours = this.hours + 12;
+        }
     }
 
     void display() {
-        System.out.println("Time = "+0);
+        System.out.println("Time = " + this.hours + ":" + this.minutes + ":" + this.seconds);
     }
 }
