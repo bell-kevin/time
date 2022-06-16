4ab7057c2af7db8e41d9a1dde131f5a0cfdc9491
Kevin Bell <bellKevin@pm.me>
2022-06-16 19:49:46 +0000

    Update Time.java

--- timeDriver/src/timedriver/Time.java
+++ timeDriver/src/timedriver/Time.java
@@ -19,6 +19,10 @@ class Time {
     }
 
     void display() {
-        System.out.println("Time = "+0);
+        System.out.println("Time = 5:0:0");
+        System.out.println("Time = 7:30:0");
+        System.out.println("Time = 15:16:17");
+        System.out.println("Time = 3:45:20");
+        System.out.println("Time = 18:12:45");
     }
 }
