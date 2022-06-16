b60f25e41b5372b35be4e2f5795786d8a4dc1902
Kevin Bell <bellKevin@pm.me>
2022-06-16 20:05:37 +0000

    Update TimeDriver.java

--- timeDriver/src/timedriver/TimeDriver.java
+++ timeDriver/src/timedriver/TimeDriver.java
@@ -4,15 +4,15 @@ public class TimeDriver {
 
     public static void main(String[] args) {
         System.out.println("Ch 7 Time by Kevin Bell\n");
-        Time onlyHour=new Time(5);
-        Time HourMin=new Time(7,30);
-        Time HourMinSec=new Time(15,16,17);
-        Time AM=new Time(3,45,20,"am");
-        Time PM=new Time(6,12,45,"pm");
+        Time onlyHour = new Time(5);
+        Time HourMin = new Time(7, 30);
+        Time HourMinSec = new Time(15, 16, 17);
+        Time AM = new Time(3, 45, 20, "am");
+        Time PM = new Time(6, 12, 45, "pm");
         onlyHour.display();
         HourMin.display();
         HourMinSec.display();
         AM.display();
         PM.display();
-    }  
+    }
 }
