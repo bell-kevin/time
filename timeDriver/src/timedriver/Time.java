6ebe003322c3b1cacb79c6f579a8c15d61daaaa2
Kevin Bell <bellKevin@pm.me>
2022-06-16 19:31:17 +0000

    Update Time.java

--- timeDriver/src/timedriver/Time.java
+++ timeDriver/src/timedriver/Time.java
@@ -1,22 +1,24 @@
 package timedriver;
+
 class Time {
+
     Time(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     Time(int i, int i0) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     Time(int i, int i0, int i1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     Time(int i, int i0, int i1, String am) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     void display() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }  
+
+    }
 }
